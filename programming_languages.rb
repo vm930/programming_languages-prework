b0a4d6a18c2d62fdf84a77eae3a6245f994c1bed
abstract-animation-9914 <vm930@hotmail.com>
2019-02-11 14:37:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,26 +1,26 @@
-require "pry"
-def reformat_languages(languages)
-   new_hash = {}
-  languages.each do |style,language_hash|
-# binding.pry
-# # # :oo => {} 
-  language_hash.each do |language, attribute_hash|
-    #:ruby => {}
-    attribute_hash.each do |type, str|
-      #interpreted or complied
-      if new_hash[language].nil?
-        new_hash[language] = {}
-      end 
-        new_hash[language][:style] ||= []  #only assign to values that are not already been assign
-        new_hash[language][:style] << style
-      if new_hash[language][type].nil?
-        new_hash[language][type] = str
-      end
-    end 
-  end 
-end 
-  retrun new_hash
-end 
+# require "pry"
+# def reformat_languages(languages)
+#   new_hash = {}
+#   languages.each do |style,language_hash|
+# # binding.pry
+# # # # :oo => {} 
+#   language_hash.each do |language, attribute_hash|
+#     #:ruby => {}
+#     attribute_hash.each do |type, str|
+#       #interpreted or complied
+#       if new_hash[language].nil?
+#         new_hash[language] = {}
+#       end 
+#         new_hash[language][:style] ||= []  #only assign to values that are not already been assign
+#         new_hash[language][:style] << style
+#       if new_hash[language][type].nil?
+#         new_hash[language][type] = str
+#       end
+#     end 
+#   end 
+# end 
+#   retrun new_hash
+# end 
 
 
 
