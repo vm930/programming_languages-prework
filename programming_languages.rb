6b88e0ea16490f4198933b766e4e4245b17def3b
abstract-animation-9914 <vm930@hotmail.com>
2019-02-11 14:31:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
    new_hash = {}
   languages.each do |style,language_hash|
 # binding.pry
-# # # :oo => {}   => {:ruby=>{
+# # # :oo => {} 
   language_hash.each do |language, attribute_hash|
     #:type => {}
     attribute_hash.each do |type, str|
